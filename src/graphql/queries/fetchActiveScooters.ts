9d758db7be7c90b135d0483f396b4b8c1847d52b
Akif <akifshah7@gmail.com>
2025-04-16 16:34:05 +0530

    map integrated

--- src/graphql/queries/fetchActiveScooters.ts
+++ src/graphql/queries/fetchActiveScooters.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const FETCH_ACTIVE_SCOOTERS = gql`
   query fetchActiveScooters {
-    scooters(where: {is_active: {_eq: true}}) {
+    scooters(where: { is_active: { _eq: true } }) {
       registration_number
       is_active
       id
@@ -11,6 +11,8 @@ export const FETCH_ACTIVE_SCOOTERS = gql`
           full_name
         }
       }
+      latitude
+      longitude
     }
   }
 `;
--- src/modules/home/components/ScootersMap.tsx
+++ src/modules/home/components/ScootersMap.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
-const ScootersMap: React.FC = () => {
+type Props = {
+  markers: { lat: number; lng: number }[];
+};
+
+const containerStyle = {
+  width: "100%",
+  height: "40vh",
+  borderRadius: "0.5rem",
+};
+
+const center = {
+  lat: 28.6139,
+  lng: 77.2090,
+};
+
+const mapOptions = {
+  disableDefaultUI: true,
+  draggable: true,
+  scrollwheel: true,
+  disableDoubleClickZoom: false,
+  keyboardShortcuts: false,
+};
+
+const ScootersMap: React.FC<Props> = ({ markers }) => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
+  });
+
   return (
-    <div className='w-full flex items-center justify-center h-[40vh] bg-gray-300 rounded-lg'>
-      {'[ Map ]'}
+    <div className="w-full flex items-center justify-center">
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={12}
+          options={mapOptions}
+        >
+          {/* Render each marker */}
+          {markers?.map((marker, index) => (
+            <Marker key={index} position={marker} />
+          ))}
+        </GoogleMap>
+      ) : (
+        <div className="w-full h-[40vh] bg-gray-300 rounded-lg flex items-center justify-center">
+          Loading map...
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ScootersMap
+export default ScootersMap;
