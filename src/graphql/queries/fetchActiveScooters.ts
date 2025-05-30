7ba295a4a77d856738435a543f317dee658868e1
Akif <akifshah7@gmail.com>
2025-05-30 15:48:27 +0530

    Revert "Revert "Revert "changed apis"""
    This reverts commit 42a4fe4510d3881d416fad5ae8156b7fa3ac8d04.

--- src/graphql/queries/fetchActiveScooters.ts
+++ src/graphql/queries/fetchActiveScooters.ts
@@ -2,29 +2,17 @@ import { gql } from "@apollo/client";
 
 export const FETCH_ACTIVE_SCOOTERS = gql`
   query fetchActiveScooters {
-    scootersCollection(first: 50, filter: { is_active: { eq: true } }) {
-      edges {
-        node {
-          registration_number
-          is_active
-          id
-          latitude
-          longitude
-          ride_detailsCollection {
-            edges {
-              node {
-                users {
-                  full_name
-                }
-              }
-            }
-          }
+    scooters(where: { is_active: { _eq: true } }) {
+      registration_number
+      is_active
+      id
+      rides {
+        user {
+          full_name
         }
       }
-      pageInfo {
-        hasNextPage
-        endCursor
-      }
+      latitude
+      longitude
     }
   }
 `;
